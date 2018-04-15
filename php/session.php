7bd57ef787c1a3d46e681e74fc6af70dc9ed6a55
e.formica <edilio95@gmail.com>
2018-04-15 23:46:39 +0200

    mysqlispecialwe

--- php/session.php
+++ php/session.php
@@ -5,6 +5,6 @@
    if (isset($_SESSION['id_cliente'])) {
        $id_cliente = $_SESSION['id_cliente'];
        $ses_sql = mysqli_query($db, "Select * from Cliente where id = '$id_cliente' ");
-       $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
+       $row = htmlspecialchars(mysqli_fetch_array($ses_sql, MYSQLI_ASSOC));
        $login_session = $row['nome'] . " " . $row['cognome'];
    }
