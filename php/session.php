ac763386f00fb0387372ffeab5aaaa9c0304a6e8
e.formica <edilio95@gmail.com>
2018-04-16 00:49:12 +0200

    we

---
+++ php/session.php
@@ -0,0 +1,10 @@
+<?php
+   include 'config.php';
+   session_start();
+   $db = Database::getInstance()->getConnection();
+   if (isset($_SESSION['id_cliente'])) {
+       $id_cliente = $_SESSION['id_cliente'];
+       $ses_sql = mysqli_query($db, "Select * from Cliente where id = '$id_cliente' ");
+       $row = htmlspecialchars(mysqli_fetch_array($ses_sql, MYSQLI_ASSOC));
+       $login_session = $row['nome'] . " " . $row['cognome'];
+   }
